a4bd8887b8552ffbd4fb213a23fdedbce867fe35
zhangds <zhangds@fiberhome.com>
2020-09-03 15:31:17 +0800

    加两个例子

---
+++ chapter01/section01/page05-03.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+# 正方形内切等边三角形
+t=Pen()
+t.setheading(90)
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+t.right(105)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+
+t.right(105)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+
+# 暂停，控制台输入任意键结束
+print('Enter any key to end the program...')
+input()
---
+++ chapter01/section01/page06-05.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+# 鱼形纹
+t=Pen()
+t.setheading(90)
+
+t.forward(80)
+t.right(120)
+t.forward(160)
+t.left(60)
+t.forward(160)
+t.right(120)
+t.forward(80)
+
+t.right(120)
+t.forward(160)
+t.left(60)
+t.forward(160)
+
+
+
+# 暂停，控制台输入任意键结束
+print('Enter any key to end the program...')
+input()
