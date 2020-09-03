d2f358c0c03216a1d70923d2eec2598c40ed456c
zhangds <zhangds@fiberhome.com>
2020-09-03 10:39:09 +0800

    两个示例

---
+++ chapter01/section01/page03-13.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+t=Pen()
+t.setheading(90)
+
+# 第13题
+t.forward(150)
+t.left(120)
+t.forward(50)
+t.left(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.left(120)
+t.forward(50)
+
+# 暂停，控制台输入任意键结束
+print('Enter any key to end the program...')
+input()
---
+++ chapter01/section01/page04-01.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+def zfxsjx():
+    t=Pen()
+    t.setheading(90)
+    t.right(30)
+    t.forward(100)
+    t.right(120)
+    t.forward(100)
+
+    t.right(30)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+
+zfxsjx()
+
+# 暂停，控制台输入任意键结束
+print('Enter any key to end the program...')
+input()
